e2b5860488477170e5bacb09d3e09f3cd6e72bd8
dIVCREATION <divya1sagar@gmail.com>
2024-04-19 17:35:27 +0530

    add app.js

---
+++ App.js
@@ -0,0 +1,12 @@
+import ScrollIndicator from './Scrolling';
+
+function App()  {
+
+    return <>
+     <ScrollIndicator url={'http://dummyjson.com/products?limit=100'}></ScrollIndicator>
+       
+        
+    
+    </>
+
+}
